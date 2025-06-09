30c4de1bab6aa8de7e70b03c415e4bfc0dfd7feb
Avril Donovan Gonzales <88221381+adgonzales18@users.noreply.github.com>
2025-06-10 04:36:23 +0800

    merge items

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import { useEffect, useState } from 'react'
 import { supabase } from './utils/supabaseClient'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-=======
-import { useState } from 'react'
-
->>>>>>> 4786a47274c4c6331ca81e8cf502deffd2968c7b
 
 function App() {
   const [session, setSession] = useState(null)
 
-<<<<<<< HEAD
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
@@ -29,13 +23,6 @@ function App() {
   else {
     return (<div>Logged in!</div>)
   }
-=======
-  return (
-    <>
-     
-    </>
-  )
->>>>>>> 4786a47274c4c6331ca81e8cf502deffd2968c7b
 }
 
 export default App
